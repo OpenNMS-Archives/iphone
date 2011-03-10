21c01661202e687f4c6c01fd8f2a4138a873c552
Benjamin Reed <rangerrick@befunk.com>
2011-03-10 08:39:41 -0500

    empty project

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OpenNMS
+//
+//  Created by Benjamin Reed on 3/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"OpenNMSAppDelegate");
+    [pool release];
+    return retVal;
+}
